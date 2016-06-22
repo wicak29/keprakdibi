193bd47a719fbcbc921030ccfe91af277be43d52
I Putu Gede Indra Gunawan <i.putu.indra13@mhs.if.its.ac.id>
2016-06-22 09:38:58 +0800

    Rancangan Kelistrikan

--- application/views/V_sidebar.php
+++ application/views/V_sidebar.php
@@ -35,6 +35,7 @@
                     <li class="sidebar-brand"><a href="#">INDIKATOR<i class="fa fa-sliders sidebar-title"></i></a></li>
                     <li><a href="<?php echo base_url('apbd/');?>"><i class="fa fa-angle-right"></i> APBD</a></li>
                     <li><a href="<?php echo base_url('pelabuhan/');?>"><i class="fa fa-angle-right"></i> Pelabuhan</a></li>
+                    <li><a href="<?php echo base_url('kelistrikan/C_kelistrikan/');?>"><i class="fa fa-angle-right"></i> Kelistrikan</a></li>
                     <li><a href="#"><i class="fa fa-angle-right"></i> Cooming soon!</a></li>
                     <li class="sidebar-brand"><a href="#">KONTAK<i class="fa fa-user sidebar-title"></i></a></li>
                     <li><a href="<?php echo base_url('C_pic/viewLihatPic');?>"><i class="fa fa-angle-right"></i> Lihat Kontak</a></li>
